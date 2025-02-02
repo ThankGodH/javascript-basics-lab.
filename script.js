3e7581caa2e23b984bb16deec0c900ddb3c5dba0
ThankGodH <n01754713@humber.ca>
2025-02-01 22:22:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+//Declaring and using Variables
+let studentName = "Michael";
+let studentAge = 25;           
+let isEnrolled = true;        
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+//String
+let firstName = "Michael";
+let lastName = "Scofield";
+console.log("Full Name:", firstName + " " + lastName);
+
+//Number
+let distance = 250;
+let largeNumber = 2487834878739273n;
+console.log("Distance:", distance);
+console.log("largeNumber:", largeNumber);
+
+//Boolean
+let Jesus = true;
+let allothergods = false;
+console.log("Jesus is God:", Jesus);
+console.log("allothergods:", allothergods);
+
+//Array
+let fruits = ["Cherry", "Orange", "clementine",];
+console.log("Fruits:", fruits);
+
+//Object
+let person = {
+    firstNamename: "Michael",
+    lastName: "Scofield",
+    age: 25,
+    isEnrolled: true
+}
+console.log("Person:", person);
+
+//Basic Operators
+
+//Arithmetic Operators
+console.log(5 + 5);
+console.log(10 - 5);
+console.log(5 * 5);
+console.log(10 / 5);
+
+//Comparison Operators
+let x = 10;
+let y = 20;
+console.log(10 > y);
+console.log(x < y);
+console.log(x === y);
+console.log(x !== y);
+
+//Logical Operators
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
